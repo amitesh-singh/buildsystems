114c2599cb21b0c50ab68061bb46d36a0d577ea3
Amitesh Singh <singh.amitesh@gmail.com>
2015-02-01 22:05:44 +0530

    cmake - added efl linking example.

--- cmake/efl/main.c
+++ cmake/efl/main.c
@@ -4,6 +4,11 @@ int main(int argc, char **argv)
 {
    elm_init(argc, argv);
 
+   Evas_Object *win = elm_win_util_standard_add("cmakeexample",
+                                                "cmake");
+   elm_win_autodel_set(win, 1);
+   evas_object_resize(win, 200, 200);
+   evas_object_show(win);
 
    elm_run();
    elm_shutdown();
